aa9f8bfe25f49ea8584afbad82284b410f157b7b
rai <mj82xx@gmail.com>
2024-05-05 15:11:55 +0900

    chore: vitestを導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  test: {
+    coverage: {
+      enabled: true,
+      provider: "istanbul",
+      include: ["src/**/*"],
+      exclude: ["**/*.stories.*"],
+    },
+    environment: "happy-dom",
+  },
 })
