f3dc51a44f41cc1e0bf1da98a96ddd6bef8bfda8
rai <mj82xx@gmail.com>
2024-05-05 14:43:33 +0900

    feat: `bun create vite`で生成された不必要なファイルをすべて削除

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+import { css } from "../styled-system/css"
+
+export const App: FC = () => {
+  return (
+    <div>
+      <h1 className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+        Hello Vite + React!
+      </h1>
+    </div>
+  )
+}
