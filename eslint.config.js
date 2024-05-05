c2271012fe54d17160328571949231e8bd29a469
rai <mj82xx@gmail.com>
2024-05-05 13:39:24 +0900

    chore: ESLintをv9へアップデート

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+// @ts-check
+
+import { FlatCompat } from "@eslint/eslintrc"
+import eslint from "@eslint/js"
+import gitignore from "eslint-config-flat-gitignore"
+import prettier from "eslint-config-prettier"
+import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js"
+import reactRecommended from "eslint-plugin-react/configs/recommended.js"
+import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
+import tsEslint from "typescript-eslint"
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+export default tsEslint.config(
+  gitignore(),
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,mts,cts}"],
+    rules: {
+      "no-undef": "off",
+    },
+  },
+  {
+    files: ["**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}"],
+    ...reactRecommended,
+    ...reactJsxRuntime,
+  },
+  ...compat.extends("plugin:react-hooks/recommended"),
+  ...compat.extends("plugin:jsx-a11y/recommended"),
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+    },
+    rules: {
+      "no-console": "error",
+    },
+  },
+)
