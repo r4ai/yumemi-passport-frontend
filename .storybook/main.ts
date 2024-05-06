ac1442f25adc764032065d2b14092d5d7c0b75f8
rai <mj82xx@gmail.com>
2024-05-06 10:31:20 +0900

    chore: accessiblity testsを導入

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
+    "@storybook/addon-a11y",
     "@storybook/addon-interactions",
   ],
   framework: {
